b8bf1b0927993720491d676c27f0fbd5e7b40c06
PeterShipp <pshipp81@aol.com>
2020-03-18 19:18:53 -0700

    Add project files.

---
+++ Lab10_4_MediaPlayer/IAudioPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_4_MediaPlayer
+{
+    
+        interface IAudioPlayer
+        {
+            void Play();
+        }
+    
+}
---
+++ Lab10_4_MediaPlayer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab10_4_MediaPlayer
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Artist prince = new Artist("Prince");
+            prince.AddToPlaylist("Purple Rain");
+            prince.AddToPlaylist("Little Red Corvette");
+            prince.AddToPlaylist("Kiss");
+            prince.AddToPlaylist("When Dove Cry");
+
+            MediaPlayer player = new MediaPlayer();
+            player.PlayMedia(prince);
+
+            AudioBook howto = new AudioBook("How to Win Friends & Influence People", "Dale Carnegie");
+            player.PlayMedia(howto);
+
+            MovieSoundTrack starWars = new MovieSoundTrack("Star Wars");
+            starWars.SelectTrack("Opening Credits Theme");
+            player.PlayMedia(starWars);
+
+        }
+    }
+}
